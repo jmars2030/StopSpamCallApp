5f94da9f09a568e4b429ed732d529a457b23159f
Suresh Kumar <sureshvkumar@SureshstooM1Max.home>
2023-08-16 01:08:45 -0400

    Initial commit

---
+++ app/src/main/java/com/playnlearn/stopspamcallapp/service/Level0CallScreeningService.kt
@@ -0,0 +1,108 @@
+@file:Suppress("HasPlatformType")
+
+package com.playnlearn.stopspamcallapp.service
+
+import android.net.Uri
+import android.provider.ContactsContract
+import android.telecom.Call
+import android.telecom.CallScreeningService
+import android.telecom.Connection
+import android.telecom.TelecomManager
+import android.telephony.PhoneNumberUtils
+import com.playnlearn.stopspamcallapp.ScreeningPreferences
+import timber.log.Timber
+import java.util.Locale
+
+class Level0CallScreeningService : CallScreeningService() {
+  companion object {
+    private val log get() = Timber.tag("📞🔫🤖")
+  }
+
+  private val prefs by lazy { ScreeningPreferences(this) }
+
+  /**
+   * Called when a new incoming or outgoing call is added which is not in the user's contact list.
+   *
+   * https://issuetracker.google.com/issues/130081372
+   */
+
+  override fun onScreenCall(details: Call.Details) {
+    if (details.callDirection == Call.Details.DIRECTION_INCOMING) {
+      // https://www.fcc.gov/call-authentication
+      when (details.callerNumberVerificationStatus) {
+        Connection.VERIFICATION_STATUS_NOT_VERIFIED -> log.w("No caller verification was performed for ${details.formattedPhoneNumber}!")
+        Connection.VERIFICATION_STATUS_FAILED -> log.e("Caller ${details.formattedPhoneNumber} FAILED verification!")
+        Connection.VERIFICATION_STATUS_PASSED -> log.i("Caller ${details.formattedPhoneNumber} is verified...")
+      }
+
+      // Always called, even for known contacts (when holding READ_CONTACTS permission, to allow
+      // screening spoofed numbers)
+      // https://issuetracker.google.com/issues/141363242
+      //
+      // Call.Details includes caller name info as of R preview 2, but this seems to always be null
+      // because the screening service is called before the lookup completes
+      // https://issuetracker.google.com/issues/151898484
+      //
+      // Handle may be null if caller id is blocked.
+      val caller = details.handle?.let { getContactName(it.schemeSpecificPart) }
+
+      val rejectDueToVerificationStatus = when (details.callerNumberVerificationStatus) {
+        Connection.VERIFICATION_STATUS_FAILED -> prefs.declineAuthenticationFailures
+        Connection.VERIFICATION_STATUS_NOT_VERIFIED -> prefs.declineUnauthenticatedCallers
+        else -> false
+      }
+
+      val rejectDueToUnknownCaller = caller == null && prefs.declineUnknownCallers
+
+      val response = if (rejectDueToVerificationStatus || rejectDueToUnknownCaller) {
+        buildRejectionResponse()
+      } else {
+        buildAcceptResponse()
+      }
+
+      respondToCall(details, response)
+    }
+  }
+
+  private fun buildAcceptResponse(): CallResponse {
+    return CallResponse.Builder().build()
+  }
+
+  private fun buildRejectionResponse(): CallResponse {
+    log.d("Reject? ${prefs.isServiceEnabled}, Notify? ${!prefs.skipCallNotification}, Log? ${!prefs.skipCallLog}")
+    return CallResponse.Builder()
+      .setDisallowCall(prefs.isServiceEnabled)
+      .setRejectCall(prefs.isServiceEnabled)
+      // These last two options don't work, calls will not be logged and notifications will not show
+      // https://issuetracker.google.com/issues/151859054
+      .setSkipNotification(prefs.skipCallNotification)
+      .setSkipCallLog(prefs.skipCallLog)
+      .build()
+  }
+
+  private fun getContactName(phoneNumber: String): String? {
+    val uri = Uri.withAppendedPath(
+      ContactsContract.PhoneLookup.CONTENT_FILTER_URI,
+      Uri.encode(phoneNumber)
+    )
+
+    val projection = arrayOf(ContactsContract.PhoneLookup.DISPLAY_NAME)
+    val cursor = contentResolver.query(uri, projection, null, null, null)
+    return cursor?.use {
+      when (cursor.moveToFirst()) {
+        true -> cursor.getString(0)
+        else -> null
+      }
+    }
+  }
+}
+
+private val Call.Details.formattedPhoneNumber: String get() {
+  return when (val phoneNumber = handle?.schemeSpecificPart) {
+    null -> "BLOCKED"
+    else -> PhoneNumberUtils.formatNumber(
+      phoneNumber,
+      Locale.getDefault().country
+    )
+  }
+}
---
+++ app/src/main/java/com/playnlearn/stopspamcallapp/ui/MainActivity.kt
@@ -0,0 +1,194 @@
+package com.playnlearn.stopspamcallapp.ui
+
+import android.Manifest
+import android.app.role.RoleManager
+import android.app.role.RoleManager.ROLE_CALL_SCREENING
+import android.content.ActivityNotFoundException
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import com.playnlearn.stopspamcallapp.R
+import com.playnlearn.stopspamcallapp.ScreeningPreferences
+import kotlinx.android.synthetic.main.activity_main.*
+import pub.devrel.easypermissions.EasyPermissions
+import timber.log.Timber
+
+class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
+    companion object {
+        private const val REQUEST_ID_BECOME_CALL_SCREENER = 1
+        private const val REQUEST_ID_REQUEST_READ_CONTACTS_PERMISSION = 1
+
+        private const val EXTRA_CONTACT_READ_PERMISSION_DENIED = "contact_permission_denied_forever"
+    }
+
+    private val roleManager by lazy { getSystemService(RoleManager::class.java) }
+    private val prefs by lazy { ScreeningPreferences(this) }
+
+    private val hasCallScreeningRole: Boolean
+        get() = roleManager.isRoleHeld(ROLE_CALL_SCREENING)
+    private val readContactsPermissionGranted: Boolean
+        get() = EasyPermissions.hasPermissions(this, Manifest.permission.READ_CONTACTS)
+    private var contactsAccessDeniedForever = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        contactsAccessDeniedForever = savedInstanceState
+            ?.getBoolean(EXTRA_CONTACT_READ_PERMISSION_DENIED, false) ?: false
+
+        addUiListeners()
+        updateUi()
+    }
+
+    @Deprecated("Deprecated in Java")
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_ID_BECOME_CALL_SCREENER) {
+            updateUi()
+
+            when (resultCode) {
+                RESULT_OK -> Timber.i("Role was granted")
+                else -> Timber.e("Role was not granted")
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        // Forward results to EasyPermissions
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(EXTRA_CONTACT_READ_PERMISSION_DENIED, contactsAccessDeniedForever)
+    }
+
+    //<editor-fold desc="EasyPermissions.PermissionCallbacks">
+    override fun onPermissionsGranted(requestCode: Int, grantedPermissions: List<String>) {
+        if (Manifest.permission.READ_CONTACTS in grantedPermissions) {
+            requestRole()
+        }
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, deniedPermissions: List<String>) {
+        // only in onPermissionsDenied() is it certain if a permission has been permanently denied
+        if (Manifest.permission.READ_CONTACTS in deniedPermissions) {
+            contactsAccessDeniedForever = EasyPermissions.permissionPermanentlyDenied(
+                this,
+                Manifest.permission.READ_CONTACTS
+            )
+        }
+    }
+    //</editor-fold>
+
+    private fun requestContactsPermission() {
+        if (contactsAccessDeniedForever) {
+            try {
+                val intent = Intent()
+                intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                intent.data = Uri.fromParts("package", this.packageName, null)
+                startActivity(intent)
+                Toast.makeText(this,
+                    R.string.enable_read_contacts_permissions_instructions, Toast.LENGTH_LONG).show()
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(this,
+                    R.string.enable_read_contacts_permissions_instructions_long, Toast.LENGTH_LONG).show()
+            }
+        } else {
+            requestPermissions(
+                arrayOf(Manifest.permission.READ_CONTACTS),
+                REQUEST_ID_REQUEST_READ_CONTACTS_PERMISSION
+            )
+        }
+    }
+
+    private fun requestRole() {
+        val intent = roleManager.createRequestRoleIntent(ROLE_CALL_SCREENING)
+        @Suppress("DEPRECATION")
+        startActivityForResult(intent,
+            REQUEST_ID_BECOME_CALL_SCREENER
+        )
+    }
+
+    private fun addUiListeners() {
+        activateButton.setOnClickListener { requestContactsPermission() }
+
+        enableSwitch.setOnCheckedChangeListener { _, enabled ->
+            prefs.isServiceEnabled = enabled
+            updateUi()
+        }
+        skipNotificationSwitch.setOnCheckedChangeListener { _, skip ->
+            prefs.skipCallNotification = skip
+            updateUi()
+        }
+        skipCallLogSwitch.setOnCheckedChangeListener { _, skip ->
+            prefs.skipCallLog = skip
+            updateUi()
+        }
+        declineUnknownCallers.setOnCheckedChangeListener { _, skip ->
+            prefs.declineUnknownCallers = skip
+            updateUi()
+        }
+        declineAuthenticationFailures.setOnCheckedChangeListener { _, skip ->
+            prefs.declineAuthenticationFailures = skip
+            updateUi()
+        }
+        declineUnauthenticatedCallers.setOnCheckedChangeListener { _, skip ->
+            prefs.declineUnauthenticatedCallers = skip
+            updateUi()
+        }
+    }
+
+    private fun updateUi() {
+        val isInstalled = readContactsPermissionGranted && hasCallScreeningRole
+        statusLabel.text = when (isInstalled) {
+            true -> getString(R.string.status_activated)
+            else -> getString(R.string.status_inactive)
+        }
+        activateButton.isVisible = isInstalled.not()
+        enableSwitch.isVisible = isInstalled
+        skipNotificationSwitch.isVisible = isInstalled
+        skipCallLogSwitch.isVisible = isInstalled
+        declineUnknownCallers.isVisible = isInstalled
+        declineAuthenticationFailures.isVisible = isInstalled
+        declineUnauthenticatedCallers.isVisible = isInstalled
+
+        enableSwitch.isChecked = prefs.isServiceEnabled
+
+        if (Build.VERSION.SDK_INT > 100) {
+            skipNotificationSwitch.isEnabled = prefs.isServiceEnabled
+            skipNotificationSwitch.isChecked = prefs.skipCallNotification
+
+            skipCallLogSwitch.isEnabled = prefs.isServiceEnabled
+            skipCallLogSwitch.isChecked = prefs.skipCallLog
+        } else {
+            skipNotificationSwitch.isEnabled = false
+            skipNotificationSwitch.isChecked = true
+            skipNotificationDescription.visibility = View.VISIBLE
+
+            skipCallLogSwitch.isEnabled = false
+            skipCallLogSwitch.isChecked = true
+            skipCallLogDescription.visibility = View.VISIBLE
+        }
+
+
+        declineUnknownCallers.isEnabled = prefs.isServiceEnabled
+        declineUnknownCallers.isChecked = prefs.declineUnknownCallers
+
+        declineAuthenticationFailures.isEnabled = prefs.isServiceEnabled
+        declineAuthenticationFailures.isChecked = prefs.declineAuthenticationFailures
+
+        declineUnauthenticatedCallers.isEnabled = prefs.isServiceEnabled
+        declineUnauthenticatedCallers.isChecked = prefs.declineUnauthenticatedCallers
+    }
+}
